5b1137ae085fa0427097e57586c04b659bb6916f
Roberto Cornejo <devrobtech@gmail.com>
2026-01-03 13:08:26 -0600

    Day 2 of Java, add two numbers

---
+++ day2.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class day2
+{
+    public static void main(String[] args)
+    {
+        // Add two numbers
+
+        int num1 = 25;
+        int num2 = 30;
+
+        System.out.println(num1 + num2);
+    }
+}
