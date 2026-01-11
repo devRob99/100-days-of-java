9883df5d9ae2b876a59eb49346b015f2adaefa4c
Roberto Cornejo <devrobtech@gmail.com>
2026-01-10 21:26:20 -0600

    Day 14 of Java, Sum of digits of an integer

---
+++ day14.java
@@ -0,0 +1,20 @@
+// Sum of digits of an integer
+import java.util.Scanner;
+
+public class day14
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 0;
+
+        while (num > 0)
+        {
+            sum += num % 10; // Adds last digit to sum
+            num /= 10; // Removes last digit from num
+        }
+
+        System.out.println(sum);
+    }
+}
